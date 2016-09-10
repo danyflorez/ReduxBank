e759dcef8af63e62771df6f87c8b591d2eceeeb8
danyflorez <agustin.florez@techandsolve.com>
2016-09-09 20:48:34 -0500

    bank redux

---
+++ app/bankActionCreators.js
@@ -0,0 +1,27 @@
+import constants from './constants';
+
+const bankActionCreators = {
+
+   depositIntoAccount(amount) {
+     return {
+       type: constants.DEPOSIT_INTO_ACCOUNT,
+       amount: amount
+     };
+   },
+
+   withdrawFromAccount(amount) {
+     return {
+       type: constants.WITHDRAW_FROM_ACCOUNT,
+       amount: amount
+     };
+   },
+
+   toggleInfo(){
+     return {
+       type: constants.TOGGLE_INFO
+     };
+   }
+
+
+};
+export default bankActionCreators;
